95ebb9654c23066643e32d0a345cb6bd65e5e4f1
Edward Tran <giaphutran012@gmail.com>
2025-06-17 22:31:28 -0700

    changed a little bit bit still error of once saved always saved

--- index.js
+++ index.js
@@ -3,37 +3,31 @@
 //The password is ILoveProgramming
 import express from "express";
 import bodyParser from "body-parser";
-import {dirname} from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
-const __dirname=dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-let correctPassword="ILoveProgramming";
-function checkPassword(req,res,next){
-let userAuthenticated=false;
-    if (req.body["password"]===correctPassword){
-       userAuthenticated=true;
-    }else{
-        userAuthenticated=false;
-    }
-    next();
+const correctPassword = "ILoveProgramming";
+function checkPassword(req, res, next) {
+  req.userAuthenticated = req.body["password"] === correctPassword;
+  next();
 }
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(checkPassword);
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/public/index.html");
-})
-app.post("/check",(req,res)=>{
-    console.log(req.userAuthenticated);
-    if (req.userAuthenticated){
-        req.userAuthenticated=false;
-        res.sendFile(__dirname+"/public/secret.html");
-    }else{
-        res.sendFile(__dirname+"/public/index.html");
-    }
-    
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+app.post("/check", (req, res) => {
+  console.log("Authenticated: " + req.userAuthenticated);
+  if (req.userAuthenticated) {
+    req.userAuthenticated=false;
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+});
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
